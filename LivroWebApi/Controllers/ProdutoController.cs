b0df8623b92cd3995ccced8bba70b5f8f32ac7af
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-13 16:44:57 -0300

    Criado o Projeto

---
+++ LivroWebApi/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LivroWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+
+       
+
+        public WeatherForecastController()
+        {
+            
+        }
+
+       
+    }
+}
